694b99775c0cb23b76fd24dc1043104db50d7967
McKinsey Leckenby <91347096+mckinsey-leckenby@users.noreply.github.com>
2022-08-21 13:25:16 -0400

    commit tree 5e8e055e7f40fac5c7d20bb3dc613937db9ce6d0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+function submitData(name, email) {
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(res => res.json())
+        .then(obj => {
+            let p = document.createElement('p');
+            p.textContent = obj.id;
+            document.querySelector('body').appendChild(p);
+        })
+        .catch(error => {
+            document.querySelector('script').textContent = error;
+        })
+}
